fe567e2f39cbd183474edfcdf8ff619c14fa79f8
Jonathan-Ultini <jonathanultini@gmail.com>
2024-09-13 13:40:44 +0200

    added AppTitle.vue

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1>{{ title }}</h1>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: "Welcome to My Vue App"
+    };
+  }
+};
+</script>
+
+<style scoped>
+h1 {
+  font-size: 2.5rem;
+  text-align: center;
+  color: #333;
+}
+</style>
